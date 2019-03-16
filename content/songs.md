5d5d0773a6f0d72de11ff97a9c56d54b08a7396c
ben <ben.spoor@gmail.com>
2019-03-16 20:32:50 +0100

    Read songlist from file

--- content/songs.md
+++ content/songs.md
@@ -1,17 +1,12 @@
 ---
-title: "Songs"
+title: "Red Jackets Jazzband"
 tagline: "Songs"
 date: 2019-03-16T16:26:50+01:00
 ---
 
 <script src="/script/abcjs_midi_5.6.5-min.js" type="text/javascript"></script>
 <script src="/script/render_abc.js" type="text/javascript"></script>
-<script type="text/javascript" onload="loadSongs()"></script>
 
-
-<div id="songslist">
-<a href="#" onclick="renderAbcFile('/songs/little_light.abc')" >little light</a> |
-<a href="#" onclick="renderAbcFile('/songs/closer_walk.abc')" >Just a closer walk</a> |
-</div>
+<div id="songslist"></div>
 <div id="notation" style="background:#ffffee"></div>
 
--- static/script/render_abc.js
+++ static/script/render_abc.js
@@ -1,4 +1,9 @@
-function renderAbcFile(file)
+function renderAbcFile(text)
+{
+    ABCJS.renderAbc('notation', text);
+}
+
+function readFile(file, callback)
 {
     var f = new XMLHttpRequest();
     f.open("GET", file, false);
@@ -9,13 +14,30 @@ function renderAbcFile(file)
             if(f.status === 200 || f.status == 0)
             {
                 var res= f.responseText;
-                ABCJS.renderAbc('notation', res);
+                callback(res);
             }
         }
     }
     f.send(null);
 }
 
-function loadSongs() {
-  alert("Image is loaded");
+function parse_songlist(data) {
+    var songs = data.split('\n');
+    var number_of_songs = songs.length - 1;
+
+    var div = document.getElementById('songslist');
+
+    for (var i = 0; i < number_of_songs; i++) {
+        var song_name = songs[i].split(",")[0];
+        var song_path = songs[i].split(",")[1];
+
+        div.innerHTML += "<a href=\"#\" onclick=\"readFile('" + song_path + "', renderAbcFile)\" >" + song_name +"</a> | "
+    }
 }
+
+function loadSongs() {
+    readFile('_index_of_songs', parse_songlist)
+}
+
+window[ addEventListener ? 'addEventListener' : 'attachEvent' ]( addEventListener ? 'load' : 'onload', loadSongs )
+
