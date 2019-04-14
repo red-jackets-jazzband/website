d8df04b6cf5829f87f4eee83c3f735e91f0b2eea
ben <ben.spoor@gmail.com>
2019-04-14 09:42:36 +0200

    Cleanup Render Agenda JS

--- static/script/render_agenda.js
+++ static/script/render_agenda.js
@@ -1,61 +1,61 @@
-function readFile(file, callback)
-{
+"use strict";
+
+function readFile(file, callback) {
     var f = new XMLHttpRequest();
     f.open("GET", file, false);
-    f.onreadystatechange = function ()
-    {
-        if(f.readyState === 4)
-        {
-            if(f.status === 200 || f.status == 0)
-            {
-                var res= f.responseText;
+    f.onreadystatechange = function () {
+        if (f.readyState === 4) {
+            if (f.status === 200 || f.status === 0) {
+                var res = f.responseText;
                 callback(res);
             }
         }
-    }
+    };
     f.send(null);
+}
+
+function render_show(show) {
+    var table = document.getElementById("showTable");
+
+    var now = new Date();
+
+    var show_date_text = show.split(",")[0];
+    var last_day = show_date_text.split("-")[1];
+    last_day = last_day === undefined? "" : "-" + last_day;
+    var show_date = new Date(Date.parse(show_date_text.split("-")[0]));
+
+    var show_name = show.split(",")[1];
+    var show_loc = show.split(",")[2];
+
+    var topRow = table.insertRow(-1);
+    if (show_date > now) {
+        topRow.classList.add('upcomingShow');
+    }
+
+    var dateCell = topRow.insertCell(-1);
+    dateCell.innerHTML = show_date.getDate() + last_day + "/" + show_date.getMonth() + "/" + show_date.getFullYear();
+    dateCell.classList.add('dateCell');
+
+    var nameCell = topRow.insertCell(-1);
+    nameCell.innerHTML = show_name;
+    nameCell.classList.add('nameCell');
+
+    var bottomRow = table.insertRow(-1);
+    if (show_date > now) {
+        bottomRow.classList.add('upcomingShow');
+    }
+    bottomRow.insertCell(-1);
+    var locationCell = bottomRow.insertCell(-1);
+    locationCell.innerHTML = show_loc;
 }
 
 function parse_showlist(data) {
     var shows = data.split('\n');
-    var number_of_shows = shows.length - 1;
-
-    var table = document.getElementById("showTable");
-
-    var now = new Date()
-
-    for (var i = 0; i < number_of_shows; i++) {
-        var show_date_text = shows[i].split(",")[0];
-        var last_day = show_date_text.split("-")[1]
-        last_day = last_day == undefined ? "" : "-" + last_day
-        var show_date = new Date( Date.parse(show_date_text.split("-")[0]));
-
-        var show_name = shows[i].split(",")[1];
-        var show_loc = shows[i].split(",")[2];
-
-        var row = table.insertRow(-1);
-        if( show_date > now ) {
-            row.classList.add('upcomingShow')
-        }
-        var cell = row.insertCell(-1);
-        cell.innerHTML = show_date.getDate() + last_day + "/" + show_date.getMonth() + "/" + show_date.getFullYear()
-        cell.style.fontWeight = "bold";
-        cell.style.paddingRight = "5px";
-        var cell = row.insertCell(-1);
-        cell.innerHTML = show_name
-        cell.style.fontWeight = "bold";
-        var row = table.insertRow(-1);
-        if( show_date > now ) {
-            row.classList.add('upcomingShow')
-        }
-        row.insertCell(-1);
-        var cell = row.insertCell(-1);
-        cell.innerHTML = show_loc
-    }
+    shows.forEach(render_show);
 }
 
 function loadShows() {
-    readFile('shows.txt', parse_showlist) 
+    readFile('shows.txt', parse_showlist);
 }
 
-window[ addEventListener ? 'addEventListener' : 'attachEvent' ]( addEventListener ? 'load' : 'onload', loadShows )
+window[ addEventListener? 'addEventListener' : 'attachEvent']( addEventListener? 'load' : 'onload', loadShows);
