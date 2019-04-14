691daaf37f4bd842616cf6c65b7e536d566238d1
ben <ben.spoor@gmail.com>
2019-04-14 11:42:08 +0200

    Move parsing of line to separate function

--- static/script/render_agenda.js
+++ static/script/render_agenda.js
@@ -14,39 +14,46 @@ function readFile(file, callback) {
     f.send(null);
 }
 
+function parse_show_line(show) {
+
+    var show_date_text = show.split(",")[0];
+    var last_day = show_date_text.split("-")[1];
+
+    return {
+        name: show.split(",")[1],
+        location: show.split(",")[2],
+        date: new Date(Date.parse(show_date_text.split("-")[0])),
+        last_day: last_day === undefined? "" : "-" + last_day
+    };
+}
+
 function render_show(show) {
     var table = document.getElementById("showTable");
 
     var now = new Date();
 
-    var show_date_text = show.split(",")[0];
-    var last_day = show_date_text.split("-")[1];
-    last_day = last_day === undefined? "" : "-" + last_day;
-    var show_date = new Date(Date.parse(show_date_text.split("-")[0]));
-
-    var show_name = show.split(",")[1];
-    var show_loc = show.split(",")[2];
+    show = parse_show_line(show);
 
     var topRow = table.insertRow(-1);
-    if (show_date > now) {
+    if (show.date > now) {
         topRow.classList.add('upcomingShow');
     }
 
     var dateCell = topRow.insertCell(-1);
-    dateCell.innerHTML = show_date.getDate() + last_day + "/" + show_date.getMonth() + "/" + show_date.getFullYear();
+    dateCell.innerHTML = show.date.getDate() + show.last_day + "/" + show.date.getMonth() + "/" + show.date.getFullYear();
     dateCell.classList.add('dateCell');
 
     var nameCell = topRow.insertCell(-1);
-    nameCell.innerHTML = show_name;
+    nameCell.innerHTML = show.name;
     nameCell.classList.add('nameCell');
 
     var bottomRow = table.insertRow(-1);
-    if (show_date > now) {
+    if (show.date > now) {
         bottomRow.classList.add('upcomingShow');
     }
     bottomRow.insertCell(-1);
     var locationCell = bottomRow.insertCell(-1);
-    locationCell.innerHTML = show_loc;
+    locationCell.innerHTML = show.location;
 }
 
 function parse_showlist(data) {
